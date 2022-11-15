058276f62caf66c8dc32f19510b1f0575802b03a
CiscoSSJ <ciscocarande@gmail.com>
2022-11-15 20:46:37 +0100

    Subir archivo main.java

---
+++ main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+
+public class main {
+    public static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String cadena1, cadena2, cadenaCompleta;
+        System.out.println("Introduce una cadena");
+        cadena1 = input.nextLine();
+        System.out.println("Introduce la otra cadena");
+        cadena2 = input.nextLine();
+
+        cadenaCompleta = concatenar(cadena1, cadena2);
+        System.out.println(cadenaCompleta);
+    }
+
+    private static String concatenar(String cadena1, String cadena2){
+        String cadenaCompleta;
+        cadenaCompleta = cadena1+cadena2;
+        return cadenaCompleta;
+    }
+}
